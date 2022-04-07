938065aae152576c02facdd2838966b69723b45d
gaurav kumar <GKumar@limabean.onmicrosoft.com>
2022-04-07 16:24:44 +0530

    End Of Section 9

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -30,7 +30,7 @@ namespace API.Services
            var tokenDescriptor=new SecurityTokenDescriptor
            {
                Subject=new ClaimsIdentity(claims),
-               Expires=DateTime.Now.AddSeconds(10),
+               Expires=DateTime.Now.AddMinutes(10),
                SigningCredentials=creds
            };
            var tokenHandler=new JwtSecurityTokenHandler();
